bc72f3d3bac18309567ceb38ed0b995308c8d4cb
WesleyKmelo <wesleycamelo@gmail.com>
2023-10-23 10:14:58 -0300

    update pokemonData

--- angular-services/src/app/models/PokemonData.ts
+++ angular-services/src/app/models/PokemonData.ts
@@ -1,3 +1,8 @@
+//Lembrando que o modelo criado deve ser igual a forma que é retornado pela Api.
+//Aqui nesse caso , é selecionado apenas os valores necessários mas na ordem em que aparecem 
+//na api.
+
+
 export type Pokemon = {
     name:string,
     id:number,
